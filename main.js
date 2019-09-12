701c96e44519d2e095f8f4b8e8797638866759db
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-09-11 21:47:04 -0400

    update main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 
 var clock = setInterval(function() {
 
-    var currentTime = new Date()
+    var currentTime = new Date();
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
+
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        
 
     
     if (seconds < 10) {
@@ -28,5 +32,7 @@ var clock = setInterval(function() {
     }
 
     document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("date").innerHTML = date;
 
 }, 1000);
+
